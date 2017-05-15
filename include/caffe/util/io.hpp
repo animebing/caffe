ed0e00b295b0223328598ee61eec4b71433efa50
Bingbing <yanbing_dong@hotmail.com>
2017-05-15 19:42:59 +0800

    add interp.hpp and modify io.hpp

--- include/caffe/util/io.hpp
+++ include/caffe/util/io.hpp
@@ -134,8 +134,13 @@ inline bool ReadImageToDatum(const string& filename, const int label,
 bool DecodeDatumNative(Datum* datum);
 bool DecodeDatum(Datum* datum, bool is_color);
 
+// ----------------- bingbing copy from PSPNet ------------------------
 cv::Mat ReadImageToCVMat(const string& filename,
-    const int height, const int width, const bool is_color);
+			 const int height, const int width, const bool is_color,
+			 int* img_height=NULL, int* img_width=NULL);
+
+// cv::Mat ReadImageToCVMat(const string& filename,
+//    const int height, const int width, const bool is_color);
 
 cv::Mat ReadImageToCVMat(const string& filename,
     const int height, const int width);
